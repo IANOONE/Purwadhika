5f1ae83aa6b2dbd60c60d1c0f62f3f136cb1f937
IANOONE <86506939+IANOONE@users.noreply.github.com>
2022-11-08 15:09:38 +0700

    day1-pwd

---
+++ main/day1/datatypes.js
@@ -0,0 +1,47 @@
+// git
+// clone => menarik source code dari online repo
+// commit => changes locally (menyimpan perubahan)
+// push => push terhadapat commit (mengirim hasil commit)
+// fetch => refresh dari local terhadap online (mengetahui perubahan yg ada di online)
+// pull => menarik hasil perubahan yg terjadi di online ke local
+
+// fork => menyalin repository ke repository kita ; merubah repository orang lain melalui repository kita
+// create pull request => repository edit melakukan request terhadap repository source
+// agar terjadi perubahan di repository edit. masuk ke dalam repository source
+
+console.log("hello world");
+console.log("hello");
+
+
+// 
+let message;
+// assign / isi
+message = "HELLO";
+
+// var
+var a = "a";
+var a = "b";
+console.log(a);
+
+// let
+let b = "b"
+b = "c"
+console.log(b);
+
+
+// const
+const c = "c"
+
+
+
+const world = "world";
+const hello = "hello";
+const hello_world = hello + world
+
+console.log(hello_world);
+
+
+
+
+
+c
---
+++ main/day1/index.js
@@ -0,0 +1,75 @@
+// area of rectangle
+
+var p = 5;
+
+var l = 3;
+
+const rectangle = p * l;
+
+console.log(rectangle);
+
+// perimeter of rectangle
+var pp = 2 * p;
+var pl = 2 * l;
+
+const perimeter = pp + pl
+
+console.log(perimeter);
+
+
+// circle
+var phi = 3.14
+var r = 7
+var d = r * 2
+
+
+// diameter of circle
+const diameter = d
+
+// circumference of circle
+const circumference = 2 * phi * r
+
+// area of circle
+const area = phi * r * r
+
+
+console.log(diameter);
+console.log(circumference);
+console.log(area);
+
+
+// triangle
+var totalAngles = 180;
+var a = 60;
+var b = 45;
+let c = null;
+
+c = totalAngles - a - b;
+
+console.log(c);
+
+
+// difference between dates in days
+var a = new Date("06/11/2021");
+var b = new Date("06/11/2022");
+
+var differenceTime = b.getTime() - a.getTime();
+var differenceDays = differenceTime / (1000 * 3600 * 24);
+
+console.log(differenceDays);
+
+// convert days to years, months, and days
+var modal = 15
+
+var year = Math.floor(modal / 365)
+var month = Math.floor(modal % 365 / 30)
+var day = Math.floor(modal % 365 % 30)
+
+const result = year + " year " + month + " month " + day + " days"
+
+console.log(result);
+
+
+var modals = 15 % 365 / 30
+
+console.log(modals);
